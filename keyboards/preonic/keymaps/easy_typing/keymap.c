6bc652ff6815ae4540572b3623822ba2442a53ec
David Wursteisen <david.wursteisen+git@gmail.com>
2020-09-23 14:22:36 +0200

    Iteration deux du clavier

--- keyboards/preonic/keymaps/easy_typing/keymap.c
+++ keyboards/preonic/keymaps/easy_typing/keymap.c
@@ -55,26 +55,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_AZERTY] = LAYOUT_preonic_grid( \
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5 ,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_A,    KC_Z,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
-  KC_Q,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MINUS,KC_QUOTE,KC_H,    KC_J,    KC_K,    KC_L,    KC_M, \
-  KC_LSFT, KC_W,    KC_X,    KC_C,    KC_V,    KC_TAB, KC_ENT,KC_B,    KC_N,    KC_DOT,    KC_COMM, KC_SCLN,\
-  MO(3),KC_LCTL, KC_LALT, KC_LGUI, MO(1), KC_SPC,  KC_SPC, MO(2),  KC_BSLS,KC_SLASH,KC_EQL, KC_GRAVE \
+  KC_TAB,  KC_A,    KC_Z,    KC_E,    KC_R,    KC_T,    KC_GRAVE, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
+  KC_EQL,  KC_Q,    KC_S,    KC_D,    KC_F,    KC_G,    KC_QUOTE, KC_H,    KC_J,    KC_K,    KC_L,    KC_M, \
+  KC_LSFT, KC_LBRC, KC_W,    KC_X,    KC_C,    KC_V,    KC_ENT,  KC_B,    KC_N,    KC_MINUS,KC_UP,   KC_RBRC,\
+  FN     , KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_SLASH, KC_LEFT, KC_DOWN, KC_RIGHT \
 ),
 
 [_LOWER] = LAYOUT_preonic_grid( \
   _______,     KC_F1,       KC_F2,     KC_F3,      KC_F4,      KC_F5,      KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    _______, \
-  _______,     _______,    _______,    _______,    _______,    _______,    _______,  _______,  _______,  _______,  _______,   _______, \
-  _______,     _______,    _______,    _______,    _______,    _______,    _______,  _______,  _______,  _______,  _______,   _______, \
-  _______,     _______,    _______,    _______,    _______,    _______,    _______,  _______,  _______, _______,  _______,   _______, \
-  _______,     _______,    _______,    _______,    _______,    _______,    _______,  _______, _______,  _______, _______, _______  \
+  _______,     _______,    KC_UP,    _______,    _______,    _______,    _______,  _______,  _______,  _______,  _______,   _______, \
+  _______,     KC_LEFT,    KC_DOWN,    KC_RIGHT,    _______,    _______,    _______,  _______,  _______,  _______,  _______,   _______, \
+  _______,     _______,    _______,    _______,    _______,    _______,    _______,  _______,  KC_EQL, _______,  _______,   _______, \
+  _______,     _______,    _______,    _______,    _______,    _______,    _______,  _______, KC_BSLS,    KC_COMM,    KC_DOT,  KC_SCLN   \
 ),
 
 [_RAISE] = LAYOUT_preonic_grid( \
   _______,     KC_F1,       KC_F2,    KC_F3,       KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,   _______, \
-  MY_AGRAVE,   KC_UP,       MY_EAIGU, MY_EGRAVE,    _______,    _______,    _______,    _______,    _______,    _______,    _______,  _______, \
-  KC_LEFT,     KC_DOWN,    KC_RIGHT,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,  _______, \
-  _______,     _______,    MY_CDILLE,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,  _______, \
-  _______,     _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,  _______  \
+  _______,   _______,       KC_UP, _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,  _______, \
+  _______,     KC_LEFT,    KC_DOWN,    KC_RIGHT,    _______,    _______,    _______,    _______,    _______,    _______,    _______,  _______, \
+  _______,     _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,  _______, \
+  _______,     _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_LEFT,    KC_UP,    KC_DOWN,  KC_RIGHT  \
 ),
 
 [_FN] = LAYOUT_preonic_grid( \
@@ -90,7 +90,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 void keyboard_post_init_user(void) {
-    backlight_toggle();
+    backlight_disable();
+    rgblight_disable();
 }
 
 
@@ -116,21 +117,44 @@ bool led_update_user(led_t led_state) {
 }
 */
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (record->event.pressed) {
-        switch(keycode) {
-            case MY_AGRAVE:
-                SEND_STRING("à");
-                return false;
-            case MY_EAIGU:
-                SEND_STRING("é");
-                return false;
-            case MY_EGRAVE:
-                SEND_STRING("è");
-                return false;
-            case MY_CDILLE:
-                SEND_STRING("ç");
-                return false;
+    switch (keycode) {
+        case LOWER:
+          if (record->event.pressed) {
+            layer_on(_LOWER);
+            update_tri_layer(_LOWER, _RAISE, _FN);
+            rgblight_enable();
+            rgblight_setrgb(255, 0, 0);
+          } else {
+            layer_off(_LOWER);
+             update_tri_layer(_LOWER, _RAISE, _FN);
+             rgblight_disable();
+          }
+          return false;
+          break;
+        case RAISE:
+          if (record->event.pressed) {
+            layer_on(_RAISE);
+             update_tri_layer(_LOWER, _RAISE, _FN);
+             rgblight_enable();
+             rgblight_setrgb(0, 255, 0);
+          } else {
+            layer_off(_RAISE);
+             update_tri_layer(_LOWER, _RAISE, _FN);
+             rgblight_disable();
+          }
+          return false;
+          break;
+        case FN:
+          if (record->event.pressed) {
+            layer_on(_FN);
+             rgblight_enable();
+             rgblight_setrgb(0, 0, 255);
+          } else {
+            layer_off(_FN);
+             rgblight_disable();
+          }
+          return false;
+          break;
       }
-    }
     return true;
 };
--- keyboards/preonic/keymaps/easy_typing/rules.mk
+++ keyboards/preonic/keymaps/easy_typing/rules.mk
@@ -1,4 +1,4 @@
 TAP_DANCE_ENABLE = no
 BACKLIGHT_ENABLE  = yes # Enable keyboard backlight functionality
-
+RGBLIGHT_ENABLE = yes
 COMMAND_ENABLE = no
