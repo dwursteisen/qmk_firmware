7b2b829a5c932dedd870c95c83267ea7f866662d
David Wursteisen <david.wursteisen+git@gmail.com>
2020-10-26 22:20:45 +0100

    Turn off the light on shortcut

--- keyboards/preonic/keymaps/dudeofawesome/keymap.c
+++ keyboards/preonic/keymaps/dudeofawesome/keymap.c
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,       KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,    KC_J,    KC_F,    KC_U,    KC_P,    KC_SCLN, KC_QUOT, \
   KC_GRV,       KC_A,    KC_S,    KC_H,    KC_T,    KC_G,    KC_Y,    KC_N,    KC_E,    KC_O,    KC_I,    RSFT_T(KC_ENT), \
   KC_LSFT,      KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,    KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_UP, KC_SLSH, \
-  NUMPAD,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,       KC_SPC,       RAISE,   TT(_NUMPAD), KC_LEFT, KC_DOWN,   KC_RGHT  \
+  NUMPAD,       KC_LCTL, KC_LALT, KC_LGUI, LOWER,       KC_SPC,       RAISE,   TT(_NUMPAD), KC_LEFT, KC_DOWN,   KC_RGHT  \
 ),
 
 [_WORKMAN_P] = LAYOUT_preonic_1x2uC( \
@@ -152,7 +152,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  _______, \
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR,  KC_PIPE, \
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),KC_HOME, KC_MUTE, _______, \
-  _______, _______, _______, _______, _______,     _______,      _______, KC_MPLY, KC_VOLD, KC_VOLU,  KC_MNXT  \
+  _______, _______, _______, _______, _______,     _______,      _______, _______, KC_VOLD, KC_MPLY,  KC_VOLU  \
 ),
 
 /* Raise
@@ -173,7 +173,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_MUTE, _______, \
-  _______, _______, _______, _______, _______,     _______,      _______, KC_MPLY, KC_VOLD, KC_VOLU, KC_MNXT  \
+  _______, _______, _______, _______, _______,     _______,      _______,  _______, KC_VOLD, KC_MPLY,  KC_VOLU  \
 ),
 
 /* Adjust (Lower + Raise)
@@ -192,7 +192,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ADJUST] = LAYOUT_preonic_1x2uC( \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
   _______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, KC_DEL,  \
-  KC_CAPS, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  WORKMAN, WORKMAN_P,  COLEMAK, _______, \
+  KC_CAPS, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  WORKMAN, WORKMAN_P,  COLEMAK, BACKLIT, \
   _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______,      _______,     _______, _______, _______, _______, _______  \
 ),
@@ -280,12 +280,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return false;
     case BACKLIT:
       if (record->event.pressed) {
-        register_code(KC_RSFT);
-        #ifdef BACKLIGHT_ENABLE
-          backlight_step();
-        #endif
-      } else {
-        unregister_code(KC_RSFT);
+        rgblight_toggle();
       }
       return false;
   }
